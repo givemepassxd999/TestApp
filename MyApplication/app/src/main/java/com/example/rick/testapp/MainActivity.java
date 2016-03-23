f0dbfd6b1aea4fbd5992d9a1fc6653cd0ec96e68
Rick <givemepassxd999@gmail.com>
2016-03-23 15:32:21 +0800

    vfbsdb

--- MyApplication/app/src/main/java/com/example/rick/testapp/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/rick/testapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private String str;
@@ -20,8 +21,9 @@ public class MainActivity extends AppCompatActivity {
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                str = new String("aaa");
+                str = new String("Aa");
                 str.toLowerCase();
+                Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
             }
         });
 
