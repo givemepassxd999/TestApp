ad9bfe2686d8b950839ca4908c6a09c79259bec1
Rick <givemepassxd999@gmail.com>
2016-03-23 15:24:29 +0800

    add textview

--- MyApplication/app/src/main/java/com/example/rick/testapp/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/rick/testapp/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.rick.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String str;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.text_view);
+        textView.setText("qqqq");
     }
 }
