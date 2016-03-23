6e3fecb12528e3e732141748bd9a7f2e46e094f1
Rick <givemepassxd999@gmail.com>
2016-03-23 15:26:58 +0800

    jrgigjie

--- MyApplication/app/src/main/java/com/example/rick/testapp/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/rick/testapp/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.rick.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private String str;
     private TextView textView;
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.text_view);
         textView.setText("qqqq");
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                str.toLowerCase();
+            }
+        });
+
     }
 }
